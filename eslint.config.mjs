5e2dbe3fef0eaf7140b68dce89d2b0b20a036f19
wild-alpha <digitalaviner@gmail.com>
2026-04-26 11:45:53 +0400

    install

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-<<<<<<< HEAD
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-=======
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
->>>>>>> aee7aec24573d7a1c4d6ff8b2349b305a23ab04b
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
